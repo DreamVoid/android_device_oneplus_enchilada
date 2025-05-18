df50a617896459d6f636e5bbfffeb7b279cb2c20
流光 <i@dreamvoid.me>
2025-05-18 13:49:56 +0800

    enchilada: 更新构建属性覆盖，添加设备描述和指纹信息

--- aosp_enchilada.mk
+++ aosp_enchilada.mk
@@ -18,16 +18,12 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-# $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk) # Optional: Uncomment if you need AOSP common stuff
 
 # Inherit from enchilada device
 $(call inherit-product, device/oneplus/enchilada/device.mk)
 
 # Inherit some common PixelOS (PE-style, based on aston example) stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-
-# Inherit some common Lineage stuff.
-# $(call inherit-product, vendor/lineage/config/common_full_phone.mk) # Removed for AOSP
 
 PRODUCT_NAME := aosp_enchilada
 PRODUCT_DEVICE := enchilada
@@ -37,29 +33,10 @@ PRODUCT_MODEL := ONEPLUS A6003
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
-# PRODUCT_BUILD_PROP_OVERRIDES += \
-#     BuildDesc="OnePlus6-user 11   release-keys" \
-#     BuildFingerprint=OnePlus/OnePlus6/OnePlus6:11/RKQ1.201217.002/2111252325:user/release-keys \
-#     DeviceName=OnePlus6 \
-#     DeviceProduct=OnePlus6 \
-#     SystemDevice=OnePlus6 \
-#     SystemName=OnePlus6
-# Commented out LineageOS specific build overrides
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE=OnePlus6 \
-    TARGET_PRODUCT=OnePlus6
-
-# If you need to override build properties, do it here, for example:
-# PRODUCT_BUILD_PROP_OVERRIDES += \
-#    PRIVATE_BUILD_DESC="aosp_enchilada-userdebug $(PLATFORM_VERSION) $(BUILD_ID) $(BUILD_NUMBER) release-keys" \
-#    BUILD_FINGERPRINT=AOSP/enchilada/enchilada:$(PLATFORM_VERSION)/$(BUILD_ID)/$(BUILD_NUMBER):userdebug/release-keys
-
-# If you want to mimic PE specific features, add them here, otherwise remove/comment out.
-# For a generic AOSP, these are typically not needed unless desired.
-# TARGET_BOOT_ANIMATION_RES := 1080
-# TARGET_FACE_UNLOCK_SUPPORTED := true
-# TARGET_SUPPORTS_GOOGLE_RECORDER := false
-# TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
-# TARGET_SUPPORTS_QUICK_TAP := true
-# TARGET_USES_AOSP_RECOVERY := true
+    BuildDesc="OnePlus6-user 11   release-keys" \
+    BuildFingerprint=OnePlus/OnePlus6/OnePlus6:11/RKQ1.201217.002/2111252325:user/release-keys \
+    DeviceName=OnePlus6 \
+    DeviceProduct=OnePlus6 \
+    SystemDevice=OnePlus6 \
+    SystemName=OnePlus6
