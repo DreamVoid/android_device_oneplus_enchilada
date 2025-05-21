4dcfe13545c95b4a0b54b5e0123d936ac2d28eef
流光 <i@dreamvoid.me>
2025-05-21 22:56:51 +0800

    Initial PixelOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_enchilada.mk
+    $(LOCAL_DIR)/aosp_enchilada.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_enchilada-user \
+    aosp_enchilada-userdebug \
+    aosp_enchilada-eng
